f0dc56151352a737fb63b1a11b523b38bbeaeed1
Wesley Luz <wesleyarj@hotmail.com>
2022-11-03 13:30:23 -0300

    .

--- 00_RestWithASPNET_Scaffold/RestWithASPNET/RestWithASPNET/Startup.cs
+++ 00_RestWithASPNET_Scaffold/RestWithASPNET/RestWithASPNET/Startup.cs
@@ -4,6 +4,8 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -23,6 +25,11 @@ namespace RestWithASPNET
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddControllers();
+            services.AddSwaggerGen(c => 
+            {
+                c.SwaggerDoc
+            })
             services.AddRazorPages();
         }
 
